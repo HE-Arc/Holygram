4c72d9168530c65e7f047758112d5dcd0f9c907e
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-02-21 09:46:29 +0100

    initilize the spring boot project

---
+++ Holygram/src/main/java/ch/hearc/holygram/HolygramApplication.java
@@ -0,0 +1,13 @@
+package ch.hearc.holygram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolygramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolygramApplication.class, args);
+	}
+
+}
