deba322cc42d596391f042bc7247f1f396c0861f
Maering <salomon.segan@gmail.com>
2019-03-07 10:38:49 +0100

    Create SearchController.java

---
+++ src/main/java/ch/hearc/holygram/controllers/SearchController.java
@@ -0,0 +1,10 @@
+package ch.hearc.holygram.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SearchController {
+
+	// https://github.com/caveofprogramming/springboot/blob/master/spring-boot-tutorial-search/src/main/java/com/caveofprogramming/controllers/SearchController.java
+	// https://spring.io/guides/tutorials/bookmarks/
+}
