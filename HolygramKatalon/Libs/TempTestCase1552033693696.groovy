99cc93912acbe3c687fc90dd5ff05d58d10fc4ea
Fabien Mottier <fabien.mottier@he-arc.ch>
2019-03-08 09:33:43 +0100

    add Katalon

---
+++ HolygramKatalon/Libs/TempTestCase1552033693696.groovy
@@ -0,0 +1,22 @@
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.webui.contribution.WebUiDriverCleaner
+import com.kms.katalon.core.mobile.contribution.MobileDriverCleaner
+import com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner
+
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+
+
+RunConfiguration.setExecutionSettingFile('C:\\Users\\fabmo\\AppData\\Local\\Temp\\Katalon\\Test Cases\\TestUser\\20190308_092813\\execution.properties')
+
+TestCaseMain.beforeStart()
+
+        TestCaseMain.runTestCase('Test Cases/TestUser', new TestCaseBinding('Test Cases/TestUser',[:]), FailureHandling.STOP_ON_FAILURE , false)
+    
