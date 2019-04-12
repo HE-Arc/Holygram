ec5d65b0034159e7088a847173750dbd1666cdfe
Maering <salomon.segan@gmail.com>
2019-04-12 08:37:33 +0200

    Updated query

--- HolygramSpring/src/main/java/ch/hearc/holygram/repositories/ExorcistRepository.java
+++ HolygramSpring/src/main/java/ch/hearc/holygram/repositories/ExorcistRepository.java
@@ -12,6 +12,7 @@ import ch.hearc.holygram.models.Exorcist;
  * https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
  */
 public interface ExorcistRepository extends CrudRepository<Exorcist, Long> {
-	@Query("SELECT e FROM Exorcist e WHERE e.id IN (SELECT s.fk_exorcist FROM Service s WHERE s.fk_demon = ?1)")
+	//@Query("SELECT e FROM Exorcist e WHERE e.id IN (SELECT s.fk_exorcist FROM Service s WHERE s.fk_demon = ?1)")
+	@Query("SELECT e FROM Service s INNER JOIN Exorcist e ON s.fk_exorcist = e.id INNER JOIN Demon d ON s.fk_demon = d.id") //does not work
 	List<Exorcist> findByDemonID(Long demon_id);
 }
