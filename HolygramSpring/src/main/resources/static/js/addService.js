781f92dfe79b411cc8e145d77720d60a1b0af729
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-04-24 17:52:07 +0200

    wip validation add service

---
+++ HolygramSpring/src/main/resources/static/js/addService.js
@@ -0,0 +1,8 @@
+let addService_religion = doccument.getElementById("addService_religion");
+let addService_demon = doccument.getElementById("addService_demon");
+let addService_price = doccument.getElementById("addService_price");
+
+function validate_addService()
+{
+	return true;
+}
