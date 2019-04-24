233fa85223ee219bf4eb6aaff9a3c14ef8dc5368
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-04-24 17:08:41 +0200

    test 1

--- HolygramSpring/src/test/java/ch/hearc/holygram/models/CantonTest.java
+++ HolygramSpring/src/test/java/ch/hearc/holygram/models/CantonTest.java
@@ -1,40 +1,61 @@
-//package ch.hearc.holygram.models;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.util.Assert;
-//
-//import ch.hearc.holygram.repositories.CantonRepository;
-//
-//@RunWith(SpringRunner.class)
-//@DataJpaTest
-//public class CantonTest {
-//
-//	@MockBean
-//	private CantonRepository cr;
-//
-//	//@Test
-//	public void givenAcronymAndName_WhenCantonCreated_ThenCantonIsValid() {
-//		final String acronym = "NE";
-//		final String name = "Neuchâtel";
-//
-//		Canton c = new Canton(acronym, name);
-//		Assert.isTrue(c.getAcronym() == acronym, "Canton's acronym is invalid");
-//		Assert.isTrue(c.getName() == name, "Canton's name is invalid");
-//	}
-//
-//	//@Test
-//	public void tryInsertCanton_ThenReadIt() {
-//		final String acronym = "NE";
-//		final String name = "Neuchâtel";
-//
-//		Canton c = new Canton(acronym, name);
-//		cr.save(c);
-//
-//		Assert.isTrue(c.equals(cr.findByAcronym(acronym)), "Canton not found when just saved");
-//	}
-//
-//}
+package ch.hearc.holygram.models;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
+
+import ch.hearc.holygram.repositories.CantonRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CantonTest {
+
+	@Autowired
+	private CantonRepository cr;
+
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Test
+	public void givenAcronymAndName_WhenCantonCreated_ThenCantonIsValid() {
+		final String acronym = "NE";
+		final String name = "Neuchâtel";
+
+		Canton c = new Canton(acronym, name);
+		
+		Assert.isTrue(c.getAcronym() == acronym, "Canton's acronym is invalid");
+		Assert.isTrue(c.getName() == name, "Canton's name is invalid");
+	}
+
+	@Test
+	public void tryInsertCanton_ThenReadIt() {
+		final String acronym = "NE";
+		final String name = "Neuchâtel";
+
+		Canton c = new Canton();
+		c.setAcronym(acronym);
+		c.setName(name);
+		
+		entityManager.persist(c);
+		entityManager.flush();
+		
+		Optional<Canton> cantonRecherche = cr.findById(c.getId());
+
+		assertTrue(cantonRecherche.isPresent());
+		assertTrue(cantonRecherche.get().getId().equals(c.getId()));
+		assertThat(cantonRecherche.get()).isNotNull();
+	}
+	
+
+}
