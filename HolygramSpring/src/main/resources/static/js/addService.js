b44e4e0c884bb718e0664dcd586cafa0747bc45c
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-04-24 18:04:00 +0200

    wip evaluation

--- HolygramSpring/src/main/resources/static/js/addService.js
+++ HolygramSpring/src/main/resources/static/js/addService.js
@@ -1,8 +1,16 @@
-let addService_religion = doccument.getElementById("addService_religion");
-let addService_demon = doccument.getElementById("addService_demon");
-let addService_price = doccument.getElementById("addService_price");
+let addService_religion = document.getElementById("addService_religion");
+let addService_demon = document.getElementById("addService_demon");
+let addService_price = document.getElementById("addService_price");
+
+let error_message = document.getElementById("error_message");
+
 
 function validate_addService()
 {
-	return true;
+	let b = false;
+	b = b || addService_religion.selectedIndex != 0;
+	b = b || addService_demon.value != "";
+	b = b || addService_price.value != "";
+	error_message.hidden = b;
+	return b;
 }
