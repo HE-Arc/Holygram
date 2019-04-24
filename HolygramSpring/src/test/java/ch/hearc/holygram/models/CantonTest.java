c60e8653c71039cbb514f0e3e14847c285113f46
Maering <salomon.segan@gmail.com>
2019-04-24 15:55:05 +0200

    Update CantonTest.java

--- HolygramSpring/src/test/java/ch/hearc/holygram/models/CantonTest.java
+++ HolygramSpring/src/test/java/ch/hearc/holygram/models/CantonTest.java
@@ -1,25 +1,17 @@
 package ch.hearc.holygram.models;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.util.Assert;
 
 import ch.hearc.holygram.repositories.CantonRepository;
-import ch.hearc.holygram.seeders.CantonSeeder;
 
 @DataJpaTest
 public class CantonTest {
 
 	@MockBean
 	private CantonRepository cr;
-
-	@Before
-	public void setUp() {
-		CantonSeeder cs = new CantonSeeder();
-		cs.run();
-	}
 
 	@Test
 	public void givenAcronymAndName_WhenCantonCreated_ThenCantonIsValid() {
@@ -29,6 +21,17 @@ public class CantonTest {
 		Canton c = new Canton(acronym, name);
 		Assert.isTrue(c.getAcronym() == acronym, "Canton's acronym is invalid");
 		Assert.isTrue(c.getName() == name, "Canton's name is invalid");
+	}
+
+	@Test
+	public void tryInsertCanton_ThenReadIt() {
+		final String acronym = "NE";
+		final String name = "Neuchâtel";
+
+		Canton c = new Canton(acronym, name);
+		cr.save(c);
+
+		Assert.isTrue(c.equals(cr.findByAcronym(acronym)), "Canton not found when just saved");
 	}
 
 }
