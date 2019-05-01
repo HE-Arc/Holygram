5953c9df8e4f5a254d67c7d20e600dadd0594598
Raphael Margueron <raphael.margueron@gmail.com>
2019-05-01 15:06:58 +0200

    update test suite for jenkins

--- runTest.sh
+++ runTest.sh
@@ -4,6 +4,21 @@ Xvfb :99 &
 export local_addr="127.0.0.1:8080"
 
 /Katalon_Studio_Linux_64-5.7.1/katalon --args -noSplash -runMode=console \
-	-projectPath="HolygramKatalon" -retry=0 -testSuitePath="Test Suites/Basic" \
+	-projectPath="HolygramKatalon" -retry=0 -testSuitePath="Test Suites/AuthentificationRegistration" \
+	-executionProfile="default" -browserType="Chrome (headless)" \
+	-Djava.awt.headless
+
+/Katalon_Studio_Linux_64-5.7.1/katalon --args -noSplash -runMode=console \
+	-projectPath="HolygramKatalon" -retry=0 -testSuitePath="Test Suites/Search" \
+	-executionProfile="default" -browserType="Chrome (headless)" \
+	-Djava.awt.headless
+
+/Katalon_Studio_Linux_64-5.7.1/katalon --args -noSplash -runMode=console \
+	-projectPath="HolygramKatalon" -retry=0 -testSuitePath="Test Suites/EditExorcistProfile" \
+	-executionProfile="default" -browserType="Chrome (headless)" \
+	-Djava.awt.headless
+
+/Katalon_Studio_Linux_64-5.7.1/katalon --args -noSplash -runMode=console \
+	-projectPath="HolygramKatalon" -retry=0 -testSuitePath="Test Suites/Search" \
 	-executionProfile="default" -browserType="Chrome (headless)" \
 	-Djava.awt.headless
